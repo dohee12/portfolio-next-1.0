aa1ae88005ceeca3aefadb539e32ce31bcf41a36
dohee12 <happyddo42@gmail.com>
2025-08-27 15:53:52 +0900

    useRouter 실습

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,29 @@
-import Login from "./login/page";
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <div>
-      Home 페이지입니다.
-      <Login />
+      <div>Home</div>
+      <div className="p-8 space-y-2">
+        <div className="p-2 bg-red-300" onClick={() => router.push("/login")}>
+          로그인으로 이동
+        </div>
+        <div
+          className="p-2 bg-blue-300"
+          onClick={() => router.push("/register")}
+        >
+          로그아웃으로 이동
+        </div>
+        <div className="p-2 bg-green-300" onClick={() => router.back()}>
+          뒤로가기
+        </div>
+        <Link href={"/"}>홈으로 이동</Link>
+      </div>
     </div>
   );
 }
---
+++ app/pages/login/page.tsx
@@ -0,0 +1,5 @@
+import Client from "./ClientPage";
+
+export default function Login() {
+  return <Client />;
+}
