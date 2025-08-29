1c8fd522b311ae36e9814ab18c6b69e9aa62cad1
dohee12 <happyddo42@gmail.com>
2025-08-29 15:22:09 +0900

    feat: (pages) 경로 설정

--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,18 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
 
+  function move(path: string) {
+    router.push(path);
+  }
+
   return (
     <div>
       <div>Home</div>
       <div className="p-8 space-y-2">
-        <div className="p-2 bg-red-300" onClick={() => router.push("/login")}>
+        <div className="p-2 bg-red-300" onClick={() => move("/login")}>
           로그인으로 이동
         </div>
-        <div
-          className="p-2 bg-blue-300"
-          onClick={() => router.push("/register")}
-        >
+        <div className="p-2 bg-blue-300" onClick={() => move("/register")}>
           로그아웃으로 이동
         </div>
         <div className="p-2 bg-green-300" onClick={() => router.back()}>
