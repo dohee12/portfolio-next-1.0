060776e7793c55149a5bc67eefa5d20b601cf482
dohee12 <happyddo42@gmail.com>
2025-09-10 23:13:46 +0900

    feat : Breakword, nextImage 페이지 구현 및 video, image 삽입 및 카드 슬라이드 추가

---
+++ app/(pages)/breakWord/page.tsx
@@ -0,0 +1,21 @@
+import { Highlight, themes } from "prism-react-renderer";
+
+export default function BreakWordPage() {
+  const original = (
+    <div className="whitespace-pre-wrap">
+      {`작은 기술이라도\n선하게 쓰이면\n세상이 달라진다는 믿음.\n카카오임팩트가 존재하는\n이유입니다.`}
+    </div>
+  );
+
+  return (
+    <div className="p-20 space-y-12">
+      <div className="space-y-8">
+        <div className="text-3xl font-bold">Break Word</div>
+        <div className="text-xl font-bold">whitespace-pre-wrap</div>
+        <div className="text-xl font-bold">br</div>
+        <div className="text-xl font-bold">break-keep</div>
+        <div className="text-xl font-bold">text-balance</div>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,126 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-//import { cn } from "@/lib/utils";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-
-type PathValue = string | null;
+import { Grip } from "lucide-react";
+import { useMemo, useRef, useState } from "react";
 
 export default function Home() {
-  const router = useRouter();
+  const translateRef = useRef<HTMLDivElement>(null);
+  const [offset, setOffset] = useState(0);
+  const movingX = 500;
 
-  function move(path: PathValue) {
-    if (path != null) {
-      router.push(path);
+  const disabledNext = useMemo(() => {
+    if (!translateRef.current) {
+      return false;
     }
-  }
+
+    return translateRef.current.scrollWidth + offset - movingX <= 0;
+  }, [offset]);
+
+  const move = (dir: "prev" | "next") => {
+    if (!translateRef.current) {
+      return;
+    }
+
+    if (dir === "next" && disabledNext) {
+      return;
+    }
+
+    const delta = dir === "prev" ? 1 : -1;
+    setOffset(offset + delta * movingX);
+  };
 
   return (
-    <div>
-      <div>Home</div>
-      <div className="p-8 space-y-2">
-        <Button variant="outline" onClick={() => move("/login")}>
-          로그인으로 이동
-        </Button>
-        <Button variant="ghost" size="lg" onClick={() => move("/register")}>
-          회원가입으로 이동
-        </Button>
-        <Button variant="circle">
-          <Link href={"/login"}>Link 로그인 페이지 이동</Link>
-        </Button>
-        <Button>auth</Button>
+    <div className="">
+      <header className="flex justify-center h-[64px]">
+        <div className="w-full max-w-[1280px] flex justify-between items-center">
+          <div className="font-bold">Logo</div>
+          <div className="flex space-x-4">
+            <Button variant="ghost">ABOUT</Button>
+            <Button variant="ghost">HISTORY</Button>
+            <Button variant="ghost">PROJECT</Button>
+            <Button variant="ghost">ALGORITHM</Button>
+            <Button variant="secondary">
+              <div className="font-bold">Today I Learned</div>
+            </Button>
+          </div>
+          <div>
+            <Grip></Grip>
+          </div>
+        </div>
+      </header>
 
-        {/* <div
-          className={cn(
-            "w-100 h-100 bg-green-500 bg-red-500 bg-blue-500",
-            "text-white"  x`
-          )}
-        >
-          400px짜리 네모
-        </div> */}
+      <div>
+        {/* <section className="w-full flex justify-center bg-slate-100 h-[720px]">
+          <div className="w-full max-w-[1280px] bg-red-100 space-y-10 flex items-end">
+            <div className="w-80 text-2xl font-semibold bg-blue-200 break-keep py-10">
+              작은 기술이라도 선하게 쓰이면 세상이 달라진다는 믿음.
+              카카오임팩트가 존재하는 이유입니다.
+            </div>
+            <div className="flex-auto"></div>
+            <div className="w-200 h-full bg-amber-300">이미지 예시</div>
+          </div>
+        </section> */}
+
+        {/* <section className="w-full flex justify-center bg-slate-200 h-[720px]">
+          <div className="w-full max-w-[1280px] relative">
+            <div className="w-80 text-4xl font-semibold break-keep py-10 absolute bottom-[8px] z-10">
+              작은 기술이라도 선하게 쓰이면 세상이 달라진다는 믿음.
+              카카오임팩트가 존재하는 이유입니다.
+            </div>
+            <div className="absolute w-full h-full">
+              <video
+                className="h-[720px]"
+                autoPlay
+                loop
+                muted
+                src="https://t1.daumcdn.net/impact/Editor/20250822/20250822104919/13fde1b056f342eba0058f91318156db"
+              ></video>
+            </div>
+          </div>
+        </section> */}
+        {/* <section className="w-full flex justify-center bg-slate-200 h-[720px]">
+          <div className="w-full max-w-[1280px] relative">
+            <div className="w-80 text-4xl font-semibold break-keep py-10 absolute bottom-[8px] z-10">
+              작은 기술이라도 선하게 쓰이면 세상이 달라진다는 믿음.
+              카카오임팩트가 존재하는 이유입니다.
+            </div>
+            <div className="absolute right-0">
+              <div className="relative w-[800px] h-[720px]">
+                <Image
+                  src={"/images/bitcoin.png"}
+                  alt={"dlafdaf"}
+                  fill
+                  className="object-cover object-center"
+                ></Image>
+              </div>
+            </div>
+          </div>
+        </section> */}
+        <section className="w-full flex justify-center">
+          <div className="w-full max-w-[1280px] bg-red-100">
+            <Button disabled={offset >= 0} onClick={() => move("prev")}>
+              {"<"}
+            </Button>
+            <Button disabled={disabledNext} onClick={() => move("next")}>
+              {">"}
+            </Button>
+            <div
+              ref={translateRef}
+              className="w-full flex overflow-x-visible space-x-8 will-change-transform"
+              style={{
+                transform: `translateX(${offset}px)`,
+                transition: "transform .35s ease",
+              }}
+            >
+              {new Array(16).fill(0).map((_ele, i) => (
+                <div key={i} className="min-w-[300px] h-[200px] bg-slate-100">
+                  {i + 1}.카드
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
